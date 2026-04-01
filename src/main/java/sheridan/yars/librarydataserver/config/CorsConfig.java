efa5efc8a2d7ed95d53af7a1bf689dc58914d500
Saba Yar <yars@sheridancollege.ca>
2026-03-31 23:54:45 -0400

    fixed CorsConfig.java

--- src/main/java/sheridan/yars/librarydataserver/config/CorsConfig.java
+++ src/main/java/sheridan/yars/librarydataserver/config/CorsConfig.java
@@ -5,10 +5,10 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer{
+public class CorsConfig implements WebMvcConfigurer {
 
-
-    public void addCorsMapping(CorsRegistry registry) {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOriginPatterns("http://localhost:*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
