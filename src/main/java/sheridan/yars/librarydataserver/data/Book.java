0d2d521b9e7cc3152d0fd23984dbd7c6ae8a9b87
Saba Yar <yars@sheridancollege.ca>
2026-04-14 12:09:40 -0400

    Fixed validation issue in Book.java

--- src/main/java/sheridan/yars/librarydataserver/data/Book.java
+++ src/main/java/sheridan/yars/librarydataserver/data/Book.java
@@ -2,8 +2,10 @@ package sheridan.yars.librarydataserver.data;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.*;
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Entity
@@ -23,7 +25,7 @@ public class Book {
     private String bookTitle;
     @Positive(message = "Page count should be 1 page minimum")
     private int pageCount;
-    @NotBlank(message = "Book availability notice required")
+    @NotNull(message = "Book availability notice required")
     private boolean isAvailable;
     @PositiveOrZero(message = "Late fee must be $0 USD or higher")
     private double lateFeeUsd;
