155f6fcfefc37d9629e7a8c774dad83ade8b40f8
Saba Yar <yars@sheridancollege.ca>
2026-04-14 06:09:26 -0400

    Added book validation to Book.java

--- src/main/java/sheridan/yars/librarydataserver/data/Book.java
+++ src/main/java/sheridan/yars/librarydataserver/data/Book.java
@@ -2,6 +2,8 @@ package sheridan.yars.librarydataserver.data;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
+
 import lombok.*;
 
 @Entity
@@ -13,10 +15,16 @@ import lombok.*;
 
 public class Book {
     @Id
+    @NotBlank(message = "Item ID required")
     private String itemId;
+    @NotBlank(message = "ISBN required")
     private String isbn;
+    @NotBlank(message = "Book title required")
     private String bookTitle;
+    @Positive(message = "Page count should be 1 page minimum")
     private int pageCount;
+    @NotBlank(message = "Book availability notice required")
     private boolean isAvailable;
+    @PositiveOrZero(message = "Late fee must be $0 USD or higher")
     private double lateFeeUsd;
 }
