7198e3beb8a47de257cb75127fdab566800a70a5
Saba Yar <yars@sheridancollege.ca>
2026-04-14 06:41:38 -0400

    Added addBook, deleteBook, and editBook methods with necessary import statements to BooksController.java

--- src/main/java/sheridan/yars/librarydataserver/controller/BooksController.java
+++ src/main/java/sheridan/yars/librarydataserver/controller/BooksController.java
@@ -1,5 +1,8 @@
 package sheridan.yars.librarydataserver.controller;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 import sheridan.yars.librarydataserver.data.Book;
 import sheridan.yars.librarydataserver.data.BookRepository;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,10 +12,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.List;
@@ -45,5 +44,50 @@ public class BooksController {
         return bookRepository.findById(id)
                 .map(ResponseEntity::ok)
                 .orElseThrow(() -> new NoResourceFoundException(HttpMethod.GET, null, "/api/books/" + id));
+    }
+
+    @PostMapping
+    @Operation(summary = "Add a new book", description = "Returns OK response if book object unique item ID is given")
+    @Parameters(
+            @Parameter(name = "book", description = "The Book object to add", required = true)
+    )
+    public ResponseEntity<Book> addBook(@Valid @RequestBody Book book) {
+        log.trace("addBook() is called with book={}", book);
+        if (bookRepository.existsById(book.getItemId())) {
+            throw new RuntimeException("Book with this ID already exists");
+        }
+        return ResponseEntity.ok(bookRepository.save(book));
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete an existing book", description = "Deletes the book attached to the given item ID")
+    @Parameters(
+            @Parameter(name = "id", description = "The item ID of the book to delete", required = true)
+    )
+    public ResponseEntity<Void> deleteBook(@PathVariable String id) {
+        if (!bookRepository.existsById(id)) {
+            throw new RuntimeException("Book not found with given item ID");
+        }
+        bookRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Edit an existing book", description = "Edits book object of given item ID")
+    @Parameters({
+            @Parameter(name = "id", description = "The item ID of the book to edit", required = true),
+            @Parameter(name = "editedBook", description = "The Book object with the edited information", required = true)
+    })
+    public ResponseEntity<Book> editBook(@PathVariable String id, @Valid @RequestBody Book editedBook) {
+        return bookRepository.findById(id)
+                .map(book -> {
+                    book.setIsbn(editedBook.getIsbn());
+                    book.setBookTitle(editedBook.getBookTitle());
+                    book.setPageCount(editedBook.getPageCount());
+                    book.setAvailable(editedBook.isAvailable());
+                    book.setLateFeeUsd(editedBook.getLateFeeUsd());
+                    return ResponseEntity.ok(bookRepository.save(book));
+                })
+                .orElseThrow(() -> new RuntimeException("Book not found with given item ID"));
     }
 }
